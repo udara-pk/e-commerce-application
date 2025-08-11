888a8b8ce051068c7a1bae44981bd4e40473630e
udara-pk <53249224+udara-pk@users.noreply.github.com>
2025-08-11 10:55:31 +0530

    Initial Backend

---
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -0,0 +1,32 @@
+package com.ecommerce.project.controller;
+
+import com.ecommerce.project.model.Category;
+import com.ecommerce.project.service.CategoryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories() {
+        return categoryService.getCategories();
+    }
+
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category) {
+        categoryService.createCategory(category);
+        return "Category created";
+    }
+}
---
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -0,0 +1,24 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    private List<Category> categories = new ArrayList<>();
+    private Long nextId = 1L;
+
+    @Override
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
+        categories.add(category);
+    }
+}
