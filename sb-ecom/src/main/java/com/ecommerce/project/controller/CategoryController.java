afa40849a6301070c077cd295282ea20008a1c30
udara-pk <53249224+udara-pk@users.noreply.github.com>
2025-08-13 16:01:51 +0530

    Delete Category

--- sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,10 +2,7 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,5 +25,11 @@ public class CategoryController {
     public String createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
         return "Category created";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId) {
+        String status = categoryService.deleteCategory(categoryId);
+        return status;
     }
 }
--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryService.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface CategoryService {
     List<Category> getCategories();
     void createCategory(Category category);
+    String deleteCategory(Long categoryId);
 }
